85101a285a0cbbfdf7ddcdf58d99af6af157cf31
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-18 17:45:16 -0300

    primeira-feito

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EstruturasdeRepetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            do{
+                Console.Write("Digite uma nota entre 0 e 10: ");
+                nota = int.Parse(Console.ReadLine());
+
+                if(nota < 0 || nota > 10){
+                    Console.WriteLine("Número Inválido");
+                }
+
+            } while(nota < 0 || nota > 10);
+                Console.WriteLine("Número Aceito");
+
+             
+           
+        }
+    }
+}
